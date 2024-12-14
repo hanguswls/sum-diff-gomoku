38f27368c0d8c7914ecb7e74239c1bfcf9bb37ae
hanguswls <hanguswls@naver.com>
2024-12-15 01:32:29 +0900

    feat: initialize app structure with layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
-import Game from './components/game/Game'
+import { BrowserRouter } from 'react-router-dom'
+import Footer from './components/layout/footer/Footer'
+import Header from './components/layout/header/Header'
+import Main from './components/layout/main/Main'
+import { Layout } from './components/layout/Layout.styles'
 
 function App() {
 
   return (
-    <>
-      <Game />
-    </>
+    <BrowserRouter>
+      <Layout>
+        <Header />
+        <Main />
+        <Footer />
+      </Layout>
+    </BrowserRouter>
   )
 }
 
