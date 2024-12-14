0eb91593cde804263fd89f3b69f009d75fa169d7
hanguswls <hanguswls@naver.com>
2024-12-15 01:30:09 +0900

    feat: setup stone state management  with zustand

---
+++ src/stores/useStoneStore.ts
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+import { StoneColor, StoneState, StoneType } from "../types/stone";
+import { INITIAL_STONES } from "../constants/game";
+
+const useStoneStore = create<StoneState>()((set) => ({
+  whiteStones: INITIAL_STONES,
+  blackStones: INITIAL_STONES,
+  decrementStone: (color: StoneColor, type: StoneType) => {
+    set((state) => ({
+      [`${color}Stones`]: {
+        ...state[`${color}Stones`],
+        [type]: state[`${color}Stones`][type] - 1
+      }
+    }))
+  }
+}))
+
+export default useStoneStore;
