e20db9b413ccde7d92063a132348e14880fe77c9
hanguswls <hanguswls@naver.com>
2024-12-18 05:30:13 +0900

    refactor: separate positioning logic into Stone and BoardStone components

--- src/components/game/stone/Stone.styles.ts
+++ src/components/game/stone/Stone.styles.ts
@@ -8,36 +8,35 @@ const stoneVariants = {
     background-color: #fff;
     box-shadow: inset -3px -3px 8px 3px rgba(0,0,0,0.15);
   `,
-    black: css`
-    color: white;
-    position: relative;
-    background: linear-gradient(
-      to bottom,
-      #666666 0%,
-      #4A4A4A 40%,
-      #1A1A1A 70%,
-      #000000 100%
-    );
+  black: css`
+  color: white;
+  position: relative;
+  background: linear-gradient(
+    to bottom,
+    #666666 0%,
+    #4A4A4A 40%,
+    #1A1A1A 70%,
+    #000000 100%
+  );
 
-    &::after {
-      content: '';
-      position: absolute;
-      width: 16px;
-      aspect-ratio: 1;
-      border-radius: 50%;
-      top: 10%;
-      left: 10%;
-      background-color: rgba(255, 255, 255, 0.7);
-      filter: blur(16px);
-      z-index: 1;
-    }
+  &::after {
+    content: '';
+    position: absolute;
+    width: 16px;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    top: 10%;
+    left: 10%;
+    background-color: rgba(255, 255, 255, 0.7);
+    filter: blur(16px);
+    z-index: 11;
+  }
   `
 }
 
 export const Stone = styled.div<{ color: StoneColor}>`
   align-content: center;
   border-radius: 50%;
-  z-index: 1;
   aspect-ratio: 1 / 1;
   width: 40px;
   font-weight: 700;
@@ -52,6 +51,11 @@ export const Stone = styled.div<{ color: StoneColor}>`
   @media (max-width: 768px) {
     width: 27px;
   }
+`;
+
+export const BoardStone = styled(Stone)`
+  position: absolute;
+  z-index: 10;
 `;
 
 export const StoneCount = styled.div`
