2454df096b31eed985241d331840e31c6ce8b892
hanguswls <hanguswls@naver.com>
2024-12-18 21:26:34 +0900

    feat: Add stone selection validation logic

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { BOARD_LENGTH } from "../constants/game";
+import { BOARD_LENGTH, MAX_FIRST_TURN_STONE_TYPE, WINNING_SCORE } from "../constants/game";
 import { Stone, StoneColor } from "../types/stone";
 import useStoneStore from "../stores/useStoneStore";
 import useTurnStore from "../stores/useTurnStore";
 
 function useGame() {
   const { setSelectedStone, reset: resetStoneStore } = useStoneStore();
-  const { curTurn, reset: resetTurnStore } = useTurnStore();
+  const { curTurn, isFirstTurns, reset: resetTurnStore } = useTurnStore();
   const [board, setBoard] = useState<(Stone | null)[][]>(Array.from({ length: BOARD_LENGTH }, () => Array(BOARD_LENGTH).fill(null)));
   const [winner, setWinner] = useState<StoneColor | null>(null);
 
@@ -17,14 +17,19 @@ function useGame() {
     setBoard(Array.from({ length: BOARD_LENGTH }, () => Array(BOARD_LENGTH).fill(null)));
   }
 
-  const handleStoneSelect = ({color, type}: Stone) => {
+  const handleStoneSelect = ({ color, type }: Stone) => {
     if (color !== curTurn) {
-      alert(`${curTurn} 차례입니다.`);
+      curTurn === 'white'
+      ? alert('백돌 차례입니다.')
+      : alert('흑돌 차례입니다.');
+      return;
+    }
+    if (isFirstTurns[color] && type > MAX_FIRST_TURN_STONE_TYPE) {
+      alert(`첫 수에는 ${MAX_FIRST_TURN_STONE_TYPE} 이하의 돌만 놓을 수 있습니다`);
       return;
     }
     setSelectedStone({color, type});
-    console.log(color);
-  }
+  };
 
   return {
     board,
