85139dc81e5b0ecca22f1100420daa387ad547d8
hanguswls <hanguswls@naver.com>
2024-12-20 04:00:03 +0900

    feat: show timer and action buttons only on game page

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,10 +3,11 @@ import { ActionItemGroup, HeaderContainer, IconButton, StyledLink, Timer, TitleI
 import title from '/title.svg';
 import useTimer from "../../../hooks/useTimer";
 import useGame from "../../../hooks/useGame";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Header() {
   const { displayTime } = useTimer();
+  const { pathname } = useLocation();
   const { handleRetryButtonClick } = useGame();
   const navigate = useNavigate();
 
@@ -17,17 +18,21 @@ function Header() {
         alt="합차오목 타이틀"
         onClick={() => navigate('/')}
       />
-      <Timer>{ displayTime }</Timer>
-      <ActionItemGroup>
-        <IconButton onClick={handleRetryButtonClick}>
-          <RotateCcw />
-          다시하기
-        </IconButton>
-        <StyledLink to='/rule'>
-          <BookOpen />
-          게임규칙
-        </StyledLink>
-      </ActionItemGroup>
+      {pathname === '/' &&
+        <>
+          <Timer>{ displayTime }</Timer>
+          <ActionItemGroup>
+            <IconButton onClick={handleRetryButtonClick}>
+              <RotateCcw />
+              다시하기
+            </IconButton>
+            <StyledLink to='/rule'>
+              <BookOpen />
+              게임규칙
+            </StyledLink>
+          </ActionItemGroup>
+        </>
+      }
     </HeaderContainer>
   )
 }
