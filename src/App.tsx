f7e1ab5fe766117106ecfc5c406055e8bd65ec4c
hanguswls <hanguswls@naver.com>
2024-12-20 05:08:01 +0900

    feat: add terms of service page and update app routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Layout } from './components/layout/Layout.styles'
 import Game from './pages/game/Game'
 import Rule from './pages/rule/Rule'
 import PrivacyPolicy from './pages/legal/PrivacyPolicy'
+import TermsOfService from './pages/legal/TermsOfService'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' element={<Game />} />
           <Route path='/rule' element={<Rule />} />
           <Route path='/privacy' element={<PrivacyPolicy />} />
+          <Route path='/terms' element={<TermsOfService />} />
         </Routes>
         <Footer />
       </Layout>
