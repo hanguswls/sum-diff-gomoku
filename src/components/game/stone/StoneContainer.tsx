21bff387c4d4df70383f4c8d2047f98b0319177a
hanguswls <hanguswls@naver.com>
2024-12-22 20:23:01 +0900

    feat: add visual turn indicators - add background color for current turn in StoneContainer

--- src/components/game/stone/StoneContainer.tsx
+++ src/components/game/stone/StoneContainer.tsx
@@ -1,16 +1,19 @@
 import useGame from "../../../hooks/useGame";
 import useStoneStore from "../../../stores/useStoneStore";
+import useTurnStore from "../../../stores/useTurnStore";
 import { StoneColor, StoneType } from "../../../types/stone";
 import { Stone, StoneCount, StoneCountContainer } from "./Stone.styles";
 
 function StoneContainer({ color }: { color: StoneColor }) {
   const { handleStoneSelect } = useGame();
   const { whiteStones, blackStones } = useStoneStore();
+  const { curTurn } = useTurnStore();
   const stones = (color === 'white') ? whiteStones : blackStones;
   const stoneTypes: StoneType[] = [1, 2, 3, 4, 5];
+  const isMyTurn: boolean = color === curTurn;
 
   return (
-    <StoneCountContainer>
+    <StoneCountContainer isMyTurn={isMyTurn}>
       {(stoneTypes).map(type =>
         stones[type] > 0 &&
           <StoneCount key={type}>
